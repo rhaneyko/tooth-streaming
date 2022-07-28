d6e17dd937cccd8c541a7fdf7af2bacd9f82ac9e
rhaneyko <rhaneykohonorio@gmail.com>
2022-07-28 13:48:55 -0400

    React App

---
+++ src/pages/HomePage/Upcoming/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import {
+    Container,
+    Title,
+} from './styles'
+
+
+const Upcoming: React.FC = () => {
+
+    
+    return (
+        <Container>
+            <Title>Upcoming</Title>
+        </Container>
+    )
+}
+
+export default Upcoming;
