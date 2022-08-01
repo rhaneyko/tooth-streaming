18fbe97e0f7bbe8784c8a0c4d5483f80ee787cee
rhaneyko <rhaneykohonorio@gmail.com>
2022-08-01 16:19:20 -0400

    React App

--- src/pages/MoviePage/MovieLatest/index.jsx
+++ src/pages/MoviePage/MovieLatest/index.jsx
@@ -12,6 +12,7 @@ import {
 
 const SeriesLatest = () => {
   const [movies, setMovies] = useState([]);
+  
   useEffect(() => {
     fetch(
       'https://api.themoviedb.org/3/movie/latest?api_key=4633d4711231f27cbe562a85959df2df&language=pt-BR'
@@ -23,15 +24,15 @@ const SeriesLatest = () => {
   return (
     <Container>
        <Carousel 
-        showArrows={true} 
-        itemsToShow={3} 
-        itemsToScroll={1} 
-        disableArrowsOnEnd 
-        enableAutoPlay 
-        autoPlaySpeed={5000}>
+         showArrows={true} 
+         itemsToShow={3} 
+         itemsToScroll={1}
+         disableArrowsOnEnd 
+         enableAutoPlay 
+         autoPlaySpeed={5000}>
       {movies &&
         movies.map((movie) => (
-          <Card key={movie.id}>
+          <Card >
             <Link to={`/movies${movie.id}`}>
               <MoviesImage
                 src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
