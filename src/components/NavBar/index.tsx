4b40b8a4fdf3df72ac54b96789443f1a14bdf9aa
rhaneyko <rhaneykohonorio@gmail.com>
2022-08-02 14:16:28 -0400

    React App

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,32 +1,9 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
-
-import { Bars, Container, Nav, Logo } from './styles';
+import { Container, Logo } from './styles';
 
 const NavBar = () => {
-  const [active, setActive] = useState('nav_menu');
-  const [icon, setIcon] = useState('nav_toggler');
-  const navToggle = () => {
-    if (active === 'nav_menu') {
-      setActive('nav_menu nav_active');
-    } else setActive('nav_menu');
-
-    if (icon === 'nav_toggler') {
-      setIcon('nav_toggler');
-    } else setIcon('nav_toggler');
-  };
-
   return (
     <Container>
       <Logo>LOGO</Logo>
-      <Router>
-        <Bars onClick={navToggle} className={icon} />
-         <Nav className={active}>
-           <Link to={'/'}>Home</Link>
-           <Link to={'/movies'}>Filmes</Link>
-           <Link to={'/series'}>Series</Link>
-         </Nav>
-      </Router>
     </Container>
   );
 };
--- src/components/NavBar/styles.ts
+++ src/components/NavBar/styles.ts
@@ -1,12 +1,11 @@
 import styled from 'styled-components';
-import { FaBars } from 'react-icons/fa';
 
 export const Container = styled.header`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
-  padding: 20px 1400px 10px 50px;
-  
+
+  padding: 10px;
    
   top: 0;
   left: 0;
@@ -15,67 +14,7 @@ export const Container = styled.header`
   
   @media (max-width: 768px) {
     padding-left: 10px;
-    }
-
-  a{
-    display: flex;
-    height: 100%;
-    align-items: center;
-    font-family: 'Allerta Stencil', sans-serif;
-     
-    font-size: 1rem;
-    text-decoration: none;
-    text-transform: uppercase;
-    color: #FFF;
-
-    padding: 0 1rem;
-  
-    cursor: pointer;
-
-    }
-
-  .nav_menu {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    }
-
-  .nav_toggler {
-    display: none;
-
-    font-size: 20px;
-    }
-
-  @media screen and (max-width: 768px) {
-
-    .nav_toggler {
-    display: block;
   }
-  .nav_menu {
-    width: 50%;
-    height: 50vh;
-    flex-direction: column;
-    left: 45vh;
-    top: 8vh;
-      
-    position: absolute;
-    border: 1px solid;
-  
-    
-    background-color: ${props => props.theme.colors.navBackground};
-    transform: translateX(100%);
-    transition: 0.5s;
-    z-index: 999;
-    @media (max-width: 768px) {
-      width: 100%;
-      left: 0;
-    }
-  }
-}
-
-.nav_active {
-  transform: translateX(0%);
-}
 `;
 
 export const Logo = styled.h1`
@@ -83,25 +22,3 @@ export const Logo = styled.h1`
     margin-left: 40px;
 
 `;
-
-export const Nav = styled.nav`
-    display: flex;
-    flex-direction: row;
-`;
-
-export const Bars = styled(FaBars)`
-    display: none;
-    color: #FFF;
-
-  @media screen and (max-width: 768px) {
-    display: block;
-    position: absolute;
-    top: 0;
-    right: 0;
-    transform: translate(-100%, 75%);
-
-    font-size: 1.8rem;
-    cursor: pointer;
-}
-`;
-
