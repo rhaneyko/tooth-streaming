e63f498db550dd0b930348b1ad5423f0d9c40e2a
rhaneyko <rhaneykohonorio@gmail.com>
2022-08-04 10:43:34 -0400

    React App

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,9 +1,24 @@
+import { useState } from 'react';
+import SideBar from '../SideBar';
 import { Container, Logo } from './styles';
 
 const NavBar = () => {
+  const [active, setActive] = useState('nav_menu');
+
+  const navToggle = () => {
+    if(active === 'nav_menu') {
+      setActive('nav_menu nav_active');
+    }
+  }
+
   return (
     <Container>
       <Logo>LOGO</Logo>
+      <div className={active}
+        
+      >
+        <SideBar />
+      </div>
     </Container>
   );
 };
--- src/components/NavBar/styles.ts
+++ src/components/NavBar/styles.ts
@@ -15,6 +15,49 @@ export const Container = styled.header`
   @media (max-width: 768px) {
     padding-left: 10px;
   }
+
+  .nav_menu {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.nav_toggler {
+  font-size: 20px;
+  display: flex;
+}
+
+  .nav_toggler {
+    display: flex;
+  }
+  .nav_menu {
+    width: 486px;
+    height: 929px;
+
+    flex-direction: column;
+
+    right: 0;
+    top: 0;
+      
+    position: absolute;
+    
+    background-color: #210124;
+
+    transform: translateX(100%);
+    transition: 0.5s;
+    z-index: 999;
+
+    @media (max-width: 768px) {
+      right: 0;
+      width: 380px;
+    }
+  }
+  .nav_active {
+    transform: translateX(0);
+
+    @media (max-width: 768px) {
+      transform: translateX(0);
+    }
+  }
 `;
 
 export const Logo = styled.h1`
