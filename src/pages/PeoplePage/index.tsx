c219258c2e92c3ffc24a23bd52f27e9ba8c4ede4
rhaneyko <rhaneykohonorio@gmail.com>
2022-08-04 15:57:52 -0400

    React App

---
+++ src/pages/PeoplePage/index.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react'
+
+const PeoplePage = () => {
+    const [people, setPeople] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.themoviedb.org/3/person/{person_id}?api_key=4633d4711231f27cbe562a85959df2df&language=pt-BR')
+            .then(res => res.json())
+            .then(data => setPeople(data.results))
+    }, []);
+
+
+
+    return(
+        <div>
+            <h1>People</h1>
+        </div>
+    )
+}
+
+export default PeoplePage;
