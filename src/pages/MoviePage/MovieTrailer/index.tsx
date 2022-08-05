65226f33e2504f67fc315fbbfda0e6529a95c07a
rhaneyko <rhaneykohonorio@gmail.com>
2022-08-05 12:38:06 -0400

    React App

--- src/pages/MoviePage/MovieTrailer/index.tsx
+++ src/pages/MoviePage/MovieTrailer/index.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import ReactPlayer from 'react-player';
+import { useRouteMatch } from 'react-router-dom';
+import { key, movieDetail } from '../../../services/api';
 
 
 import {
@@ -8,22 +10,46 @@ import {
     Player,
 } from './styles'
 
+interface Props {
+    id: string;
+}
 
+const MovieTrailer: React.FC = () => {
+    const { params } = useRouteMatch<Props>();
+  console.log(params.id);
+  const [repository, setRepository] = useState<any>(null);
+  const poster = 'http://image.tmdb.org/t/p/w342/';
 
-const MovieTrailer = () => {
-    const [trailer, setTrailer] = useState<any[]>();
+  useEffect(() => {
+    movieDetail
+      .get(
+        `${params.id}?api_key=${key}&language=pt-BR&append_to_response=videos`,
+      )
+      .then(response => setRepository(response.data));
+  }, [params.id]);
+    // const [trailer, setTrailer] = useState<any[]>();
 
-    useEffect(() => {
-        fetch(
-            'https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=4633d4711231f27cbe562a85959df2df&language=pt-BR'
-        ).then(response => response.json())
-        .then(json => setTrailer(json.results));
-    }, [])
+    // useEffect(() => {
+    //     fetch(
+    //         'https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=4633d4711231f27cbe562a85959df2df&language=pt-BR'
+    //     ).then(response => response.json())
+    //     .then(json => setTrailer(json.results));
+    // }, [])
     
     
     return(
         <Container>
-            <Player></Player>
+           {repository?.videos?.results[0]?.key === undefined ? (
+            <p>Infelizmente não temos o trailer 😢</p>
+          ) : (
+            <Player>
+              <ReactPlayer
+                url={`https://www.youtube.com/watch?v=${repository.videos.results[0].key}`}
+                width="100%"
+                height="100%"
+              />
+            </Player>
+          )}
         </Container>
     )
 }
