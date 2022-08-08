d5c408412df5e2168c749a53516520171e3d4409
rhaneyko <rhaneykohonorio@gmail.com>
2022-08-08 15:56:21 -0400

    React App

--- src/pages/MoviePage/MoviePopular/index.jsx
+++ src/pages/MoviePage/MoviePopular/index.jsx
@@ -14,9 +14,6 @@ import {
   Genres,
  } from './styles';
 
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
-
 const MoviePopular = () => {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
